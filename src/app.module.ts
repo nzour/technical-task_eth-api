519d0c490dbad286f59058f5f657cb6f354e955d
zaur <znadiraliev@yandex.ru>
2022-06-16 15:04:06 +0300

    Update AppModule, remove non-necessary AppService and AppController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CqrsModule } from '@nestjs/cqrs';
+import { SharedModule } from './shared/shared.module';
+import { WalletModule } from './wallet/wallet.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    CqrsModule,
+    SharedModule,
+    WalletModule,
+  ],
 })
 export class AppModule {}
